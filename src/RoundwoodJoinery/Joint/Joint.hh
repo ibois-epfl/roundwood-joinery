5f78a43a5a54604b74c0e0cadfb65fb600d0cc85
DamienGilliard <damien.gilliard@epfl.ch>
2026-04-07 19:13:41 +0200

    fix: indentation of private/public keywords

--- src/RoundwoodJoinery/Joint/Joint.hh
+++ src/RoundwoodJoinery/Joint/Joint.hh
@@ -18,88 +18,88 @@ namespace RoundwoodJoinery::Joinery
      */
     class JointFace
     {
-    public:
-        JointFace(Eigen::Vector3d normal, std::vector<Eigen::Vector3d> corners, double targetArea = 0.0);
-        ~JointFace() = default;
+        public:
+            JointFace(Eigen::Vector3d normal, std::vector<Eigen::Vector3d> corners, double targetArea = 0.0);
+            ~JointFace() = default;
 
-        std::vector<Eigen::Vector3d> ProjectPointsOntoFace(RoundwoodJoinery::PointCloud::PointCloud& pointCloud);
+            std::vector<Eigen::Vector3d> ProjectPointsOntoFace(RoundwoodJoinery::PointCloud::PointCloud& pointCloud);
 
-        // Getters
-        /**
-         * @brief Returns the normal vector of the joint face.
-         * @return The normal vector of the joint face.
-         */
-        Eigen::Vector3d GetNormal() const
-        {
-            return this->_normal;
-        }
+            // Getters
+            /**
+            * @brief Returns the normal vector of the joint face.
+            * @return The normal vector of the joint face.
+            */
+            Eigen::Vector3d GetNormal() const
+            {
+                return this->_normal;
+            }
 
-        std::vector<Eigen::Vector3d> GetCorners() const
-        {
-            return this->_corners;
-        }
+            std::vector<Eigen::Vector3d> GetCorners() const
+            {
+                return this->_corners;
+            }
 
-        /**
-         * @brief Returns the center point of the joint face.
-         * @return The center point of the joint face.
-         */
-        Eigen::Vector3d GetCenter() const
-        {
-            return this->_center;
-        }
+            /**
+            * @brief Returns the center point of the joint face.
+            * @return The center point of the joint face.
+            */
+            Eigen::Vector3d GetCenter() const
+            {
+                return this->_center;
+            }
 
-        /**
-         * @brief Returns the area of the joint face.
-         * @return The target area of the joint face.
-         */
-        double GetTargetArea() const
-        {
-            return this->_targetArea;
-        }
+            /**
+            * @brief Returns the area of the joint face.
+            * @return The target area of the joint face.
+            */
+            double GetTargetArea() const
+            {
+                return this->_targetArea;
+            }
 
-        /**
-         * @brief Returns the current area of the joint face.
-         * @return The current area of the joint face.
-         */
-        double GetCurrentArea() const
-        {
-            return this->_currentArea;
-        }
+            /**
+            * @brief Returns the current area of the joint face.
+            * @return The current area of the joint face.
+            */
+            double GetCurrentArea() const
+            {
+                return this->_currentArea;
+            }
 
-        /**
-         * @brief Computes the current area of the joint face based on the points from the beam's point cloud that are projected onto the face.
-         * 
-         * @param beamPointCloud The point cloud of the beam to which the joint face belongs.
-         * @param alpha The alpha parameter for the alpha shape computation, which is used to determine the outline of the projected points.
-         * @return The computed current area of the joint face.
-         */
-        double ComputeCurrentArea(PointCloud::PointCloud& beamPointCloud, double alpha = 500.0);
+            /**
+            * @brief Computes the current area of the joint face based on the points from the beam's point cloud that are projected onto the face.
+            * 
+            * @param beamPointCloud The point cloud of the beam to which the joint face belongs.
+            * @param alpha The alpha parameter for the alpha shape computation, which is used to determine the outline of the projected points.
+            * @return The computed current area of the joint face.
+            */
+            double ComputeCurrentArea(PointCloud::PointCloud& beamPointCloud, double alpha = 500.0);
 
-        /**
-         * @brief Returns the current outline of the joint face based on the points from the beam's point cloud that are projected onto the face.
-         * 
-         * @param beamPointCloud The point cloud of the beam to which the joint face belongs.
-         * @param alpha The alpha parameter for the alpha shape computation, which is used to determine the outline of the projected points.
-         * @return A vector of Eigen::Vector3d representing the current outline of the joint face.
-         */
-        std::vector<Eigen::Vector3d> GetCurrentOutline(PointCloud::PointCloud& beamPointCloud, double alpha = 500.0);
+            /**
+            * @brief Returns the current outline of the joint face based on the points from the beam's point cloud that are projected onto the face.
+            * 
+            * @param beamPointCloud The point cloud of the beam to which the joint face belongs.
+            * @param alpha The alpha parameter for the alpha shape computation, which is used to determine the outline of the projected points.
+            * @return A vector of Eigen::Vector3d representing the current outline of the joint face.
+            */
+            std::vector<Eigen::Vector3d> GetCurrentOutline(PointCloud::PointCloud& beamPointCloud, double alpha = 500.0);
 
 
-        void ApplyTransformation(Eigen::Matrix4d transformation);
+            void ApplyTransformation(Eigen::Matrix4d transformation);
 
-    private:
-        Eigen::Vector3d _normal;
-        Eigen::Vector3d _center = Eigen::Vector3d::Zero();
-        std::vector<Eigen::Vector3d> _corners;
-        std::vector<Eigen::Vector3d> _originalCorners; // A backup should it be useful...
-        double _targetArea;
-        double _currentArea;
-        std::vector<Eigen::Vector3d> _projectedPoints;
-        
-        /**
-         * @brief the outline polygon that is optional for technical reasons, but is systematically created at construction
-         */
-        std::optional<CGAL::Polygon_2<CGAL::Projection_traits_3<K>>> _outline_polygon;
+        private:
+            Eigen::Vector3d _normal;
+            Eigen::Vector3d _center = Eigen::Vector3d::Zero();
+            std::vector<Eigen::Vector3d> _corners;
+            std::vector<Eigen::Vector3d> _originalCorners; // A backup should it be useful...
+            double _targetArea;
+            double _currentArea;
+            std::vector<Eigen::Vector3d> _projectedPoints;
+            
+            /**
+            * @brief the outline polygon that is optional for technical reasons, but is systematically created at construction
+            */
+            std::optional<CGAL::Polygon_2<CGAL::Projection_traits_3<K>>> _outline_polygon;
     };
 
     /**
@@ -107,65 +107,65 @@ namespace RoundwoodJoinery::Joinery
      */
     class Joint
     {
-    public:
-        Joint(std::vector<JointFace> faces);
-        Joint() = default;
-        ~Joint() = default;
+        public:
+            Joint(std::vector<JointFace> faces);
+            Joint() = default;
+            ~Joint() = default;
 
-        /**
-         * @brief Returns the faces that make up the joint.
-         * @return A vector of JointFace objects representing the faces of the joint.
-         */
-        std::vector<JointFace> GetFaces()
-        {
-            return this->_faces;
-        }
-
-        /**
-         * @brief Returns the center point of the joint.
-         * @return The center point of the joint.
-         */
-        Eigen::Vector3d GetCenter() const
-        {
-            return this->_center;
-        }
-
-        /**
-         * @brief Returns the number of faces in the joint.
-         * @return The number of faces in the joint.
-         */
-        size_t GetNumFaces()
-        {
-            return this->_faces.size();
-        }
-
-        void SetClosestPointOnSkeleton(Eigen::Vector3d correspondance)
-        {
-            this->_closestPointOnSkeleton = correspondance;
-        }
-
-        /**
-         * @brief Returns the closest point on the skeleton to this joint. If the closest point has not been set, it returns (0,0,0) and prints a warning message.
-         * @return The closest point on the skeleton to this joint.
-         */
-        Eigen::Vector3d GetClosestPointOnSkeleton() const
-        {
-            if (this->_closestPointOnSkeleton == Eigen::Vector3d::Zero())
+            /**
+            * @brief Returns the faces that make up the joint.
+            * @return A vector of JointFace objects representing the faces of the joint.
+            */
+            std::vector<JointFace> GetFaces()
             {
-                std::cerr << "Warning: Closest point on skeleton has not been set for this joint. Returning (0,0,0) as default." << std::endl;
+                return this->_faces;
             }
-            return this->_closestPointOnSkeleton;
-        }
 
-        /**
-         * @brief A method to apply a transformation to the joint, which will be applied to all its faces.
-         * @param transformation The 4x4 transformation matrix to be applied to the joint.
-         */
-        void ApplyTransformation(Eigen::Matrix4d transformation);
+            /**
+            * @brief Returns the center point of the joint.
+            * @return The center point of the joint.
+            */
+            Eigen::Vector3d GetCenter() const
+            {
+                return this->_center;
+            }
 
-    private:
-        std::vector<JointFace> _faces;
-        Eigen::Vector3d _center = Eigen::Vector3d::Zero();
-        Eigen::Vector3d _closestPointOnSkeleton = Eigen::Vector3d::Zero();
+            /**
+            * @brief Returns the number of faces in the joint.
+            * @return The number of faces in the joint.
+            */
+            size_t GetNumFaces()
+            {
+                return this->_faces.size();
+            }
+
+            void SetClosestPointOnSkeleton(Eigen::Vector3d correspondance)
+            {
+                this->_closestPointOnSkeleton = correspondance;
+            }
+
+            /**
+            * @brief Returns the closest point on the skeleton to this joint. If the closest point has not been set, it returns (0,0,0) and prints a warning message.
+            * @return The closest point on the skeleton to this joint.
+            */
+            Eigen::Vector3d GetClosestPointOnSkeleton() const
+            {
+                if (this->_closestPointOnSkeleton == Eigen::Vector3d::Zero())
+                {
+                    std::cerr << "Warning: Closest point on skeleton has not been set for this joint. Returning (0,0,0) as default." << std::endl;
+                }
+                return this->_closestPointOnSkeleton;
+            }
+
+            /**
+            * @brief A method to apply a transformation to the joint, which will be applied to all its faces.
+            * @param transformation The 4x4 transformation matrix to be applied to the joint.
+            */
+            void ApplyTransformation(Eigen::Matrix4d transformation);
+
+        private:
+            std::vector<JointFace> _faces;
+            Eigen::Vector3d _center = Eigen::Vector3d::Zero();
+            Eigen::Vector3d _closestPointOnSkeleton = Eigen::Vector3d::Zero();
     };
 }
