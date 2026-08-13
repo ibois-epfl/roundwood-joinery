a2bfc8642da6ef8ca8f407e74c03c4cb6ac61fb0
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2026-08-14 00:02:21 +0200

    fix: typo in bindings

--- src/RoundwoodJoinery/roundwood_joinery_binding.cc
+++ src/RoundwoodJoinery/roundwood_joinery_binding.cc
@@ -122,7 +122,7 @@ NB_MODULE(roundwoodJoineryBindings, m)
              nb::arg("maxIterations") = 10, 
              nb::arg("minRelativeTranslationRMSE") = 1.0,
              nb::arg("alphaForAreaComputations") = 500.0,
-             nb::arg("outputFolderPath") = std::optional<std::string>());
+             nb::arg("outputFolderPath") = std::optional<std::string>())
   
         .def("compute_remaining_sections", &RoundwoodJoinery::Beam::Beam::ComputeRemainingSections, 
              "Compute the remaining sections of the joint faces after applying the current transformations to the beam and its joints");
