cca5ad1d4e913dd32126e6a09ef7a4978c86318e
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2026-08-15 01:23:00 +0200

    fix: small cleanup of commented lines [no ci]

--- src/RoundwoodJoinery/Utils/Utils.cc
+++ src/RoundwoodJoinery/Utils/Utils.cc
@@ -451,15 +451,11 @@ namespace RoundwoodJoinery::Utils
             if (distance < minDistance)
             {
                 minDistance = distance;
-                // secondClosestPointOnCurve1 = closestPointCurve1;
-                // secondClosestPointOnCurve2 = closestPointCurve2;
                 closestPointCurve1 = Eigen::Vector3d(closestPointOnCurve1.x(), closestPointOnCurve1.y(), closestPointOnCurve1.z());
                 closestPointCurve2 = Eigen::Vector3d(closestPointOnCurve2.x(), closestPointOnCurve2.y(), closestPointOnCurve2.z());
             }
         }
 
-        // closestPointCurve1 = Utils::FindHeightOfTriangle(closestPointCurve2, closestPointCurve1, secondClosestPointOnCurve1);
-        // closestPointCurve2 = Utils::FindHeightOfTriangle(closestPointCurve2, secondClosestPointOnCurve1, closestPointCurve1);
         return {closestPointCurve1, closestPointCurve2};
     }
 
