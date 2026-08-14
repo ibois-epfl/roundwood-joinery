0f70b7c8a3e373798e76690c27298afc91dee14f
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2026-08-14 22:50:05 +0200

    feat: update joinery transformation component so it duplicates the joinery before transforming it, and also output a visualisation of the transformed joint outlines

--- src/gh_components/RWJ_transform_joinery/code.py
+++ src/gh_components/RWJ_transform_joinery/code.py
@@ -19,9 +19,18 @@ class RWJ_transform_joinery(component):
     def RunScript(self,
             i_transformation: Rhino.Geometry.Transform,
             i_joinery: System.Collections.Generic.List[object]) -> typing.List[rwj.JointGroup]:
+        o_joint_outlines = []
+        o_joinery = []
         for joint_group in i_joinery:
-
+            duplicated_joint_group = joint_group.duplicate()
             transformation = np.reshape(np.array(i_transformation.ToDoubleArray(True)), (4, 4))
-            joint_group.apply_transformation(transformation)
+            duplicated_joint_group.apply_transformation(transformation)
+            for joint in duplicated_joint_group.get_joints():
+                for face in joint.get_faces():
+                    face_outline = Rhino.Geometry.Polyline(
+                        [Rhino.Geometry.Point3d(pt[0], pt[1], pt[2]) for pt in face.get_corners()]
+                        )
+                    o_joint_outlines.append(face_outline)
+            o_joinery.append(duplicated_joint_group)
         
-        return i_joinery
+        return [o_joinery, o_joint_outlines]
